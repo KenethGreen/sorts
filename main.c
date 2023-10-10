804786cd18177994d3a84745c3331be562cd36c2
Kenny <gjs5502998@163.com>
2023-10-10 09:12:24 +0800

    test picksort

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "bubblesort.h"
+#include "picksort.h"
+#include "sorts.h"
 int main() {
 //    int a[4]={4,3,2,1};                               // int
     float a[4] = {3.2f,3.1f,4.5f,2.3f};                 // float
 //    const char *a[] = {"hello","world","help","I"};   // string
     int n = sizeof(a)/sizeof(a[0]);
-//    bubblesort(a,n,sizeof(int),(void *) cmpInt);
-    bubblesort(a,n,sizeof(float),(void *) cmpFloat);
-//    bubblesort(a,n,sizeof(char*),(void *)cmpString);
+//    bubblesort(a,n,sizeof(int),cmpInt);
+    picksort(a,n,sizeof(float),cmpFloat);
+//    bubblesort(a,n,sizeof(char*),cmpString);
     for(int i = 0;i<n;++i)
 //        printf("%d ",a[i]);
         printf("%.1f ",a[i]);
