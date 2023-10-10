c60aa9a3625a6e5bf28dbbbb79ddddeefa15a864
Kenny <gjs5502998@163.com>
2023-10-10 09:21:29 +0800

    add some annotation

--- picksort.c
+++ picksort.c
@@ -12,22 +12,22 @@
 #include "picksort.h"
 void picksort(void *array,int numElements,size_t elementSize,int(*cmp)(const void*,const void*))
 {
-    char * elements = (char *)array;
+    char * elements = (char *)array;                                // 1 byte each offset
     unsigned char flag = 0;
     for(int i = 0;i<numElements-1;++i)
     {
-        void *currentElement = elements + i * elementSize;
-        void *min = currentElement;
+        void *currentElement = elements + i * elementSize;          // pointer offset i step
+        void *min = currentElement;                                 // initial min element pointer
         for(int j = i+1;j<numElements;++j)
         {
-            void *searchElement = elements + j * elementSize;
-            if(cmp(min,searchElement)>0)
+            void *searchElement = elements + j * elementSize;       // pointer offset i step
+            if(cmp(min,searchElement)>0)                            // here is a new min element
             {
-                flag = 1;
-                min = searchElement;
+                flag = 1;                                           // find new min element behind the currentElement
+                min = searchElement;                                // min points at the current min one
             }
         }
-        if(flag)
+        if(flag)                                                    // if here found element that less than currentElement, swap them
         {
             char *temp = (char *) malloc(elementSize);
             memcpy(temp,min,elementSize);
--- picksort.h
+++ picksort.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file           : picksort.h
   * @author         : Kenny
-  * @brief          : None
+  * @brief          : declare quicksort function
   * @attention      : None
   * @date           : 2023/10/10
   ******************************************************************************
@@ -11,6 +11,14 @@
 
 #ifndef SORTS_PICKSORT_H
 #define SORTS_PICKSORT_H
-
+/**
+  * @brief  quick sort for int-type, float-type and string
+  * @note   None
+  * @param  array:              the array need to be sort
+  * @param  numElements:        the number of elements in array
+  * @param  elementSize:        the bits of each element in memory
+  * @param  cmp:                a function pointer which compares with two elements
+  * @retval None
+  */
 void picksort(void *array,int numElements,size_t elementSize,int(*cmp)(const void*,const void*));
 #endif //SORTS_PICKSORT_H
