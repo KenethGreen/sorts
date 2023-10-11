2965e3eee63efbdc0a9ed15b007e8b6996dd6c07
Kenny <gjs5502998@163.com>
2023-10-11 14:45:50 +0800

    testing quicksort

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "bubblesort.h"
 #include "insertsort.h"
 #include "quicksort.h"
 #include "picksort.h"
 #include "sorts.h"
 int main() {
-//    int a[4]={4,3,2,1};                               // int
-    float a[4] = {3.2f,3.1f,4.5f,2.3f};                 // float
-//    const char *a[] = {"hello","world","help","I"};   // string
-    int n = sizeof(a)/sizeof(a[0]);
-//    bubblesort(a,n,sizeof(int),cmpInt);
-//    insertsort(a,n,sizeof(float),cmpFloat);
-    quicksort(a,n,sizeof(float),0,n-1,cmpFloat);
-//    bubblesort(a,n,sizeof(char*),cmpString);
+    srand(time(0));
+    int n = 100;
+    int a[100];
+    int max = 100;
+    int min = 0;
     for(int i = 0;i<n;++i)
-//        printf("%d ",a[i]);
-        printf("%.1f ",a[i]);
-//        printf("%s ",a[i]);
+    {
+        a[i]=(rand() % (max - min + 1)) + min;
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    quicksort(a,n,sizeof(int),0,n-1,cmpInt);
+    for(int i = 0;i<n;++i)
+    {
+        if(!(i%10)&&i>0)
+            printf("\n");
+        printf("%d ",a[i]);
+    }
     return 0;
 }
