a5898f7972fb99e2a12352a6033b87d4f32c6cc6
Kenny <gjs5502998@163.com>
2023-10-15 21:45:44 +0800

    test mergesort function

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "insertsort.h"
 #include "quicksort.h"
 #include "picksort.h"
+#include "mergesort.h"
 #include "sorts.h"
 int main() {
     srand(time(0));
@@ -18,12 +19,21 @@ int main() {
         printf("%d ",a[i]);
     }
     printf("\n");
-    quicksort(a,n,sizeof(int),0,n-1,cmpInt);
+    mergesort(&a,n,sizeof(int),cmpInt);
     for(int i = 0;i<n;++i)
     {
         if(!(i%10)&&i>0)
             printf("\n");
         printf("%d ",a[i]);
     }
+    for(int i = 0;i<n-1;++i)
+    {
+        if(a[i]>a[i+1])
+        {
+            printf("\nfalse\n");
+            return 0;
+        }
+    }
+    printf("\ntrue\n");
     return 0;
 }
